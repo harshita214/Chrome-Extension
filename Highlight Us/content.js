b10842e4760a2e5eb72f561d38b11f3ce9c79594
Suman Mondal <suman.mandal1020@gmail.com>
2022-02-03 02:26:24 +0530

    add readme

--- Highlight Us/content.js
+++ Highlight Us/content.js
@@ -2,6 +2,9 @@ chrome.runtime.onMessage.addListener (
     (request, sender, sendResponse) => {
         console.log(request);
         if (request.action === "highlight") {
+            
+            console.log('Hello World');
+
             let noLinks = document.links;
             for (let i = 0; i < noLinks.length; i++) {
                 noLinks[i].style = "background-color: yellow;"
