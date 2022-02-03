a0e135e643a82244259d16820f0493662c38983d
Mahim Kapoor <96693922+mahimk9@users.noreply.github.com>
2022-02-04 02:15:38 +0530

    Updated content.js

--- PrankYourFriend/content.js
+++ PrankYourFriend/content.js
@@ -4,12 +4,20 @@ console.log("Chrome extension go?");
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
+let funnyim = [ "https://pbs.twimg.com/profile_images/1287289686839291905/HI3zi0AC_400x400.jpg" ];
+const imgs = document.getElementsByTagName("img");
+
 function gotMessage(message, sender, sendResponse) {
     console.log(message.txt);
     const headers = document.getElementsByTagName('p');
     for(let i=0;i<headers.length;i++) {
     headers[i].innerText="nityodaynityodaynityodaynityoday";
-}
+    }
+    for(let i=0; i<imgs.length; i++)
+    {
+        const randomImg = Math.floor(Math.random() * funnyim.length);
+        imgs[i].src = funnyim[randomImg];
+    }
 }
 
 
