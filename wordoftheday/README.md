e3e8d799cd66610329501365078377aee320c3fa
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-02-07 13:15:04 +0530

    background and button added

--- wordoftheday/README.md
+++
@@ -1,8 +0,0 @@
-
-# word of the day
-
-whenever user clicks on this extension it generates a random word and give its meaning
-
-![image](https://user-images.githubusercontent.com/75252077/152735211-c1eba1f5-d49b-4899-8908-79f9f94dcb74.png)
-
-https://user-images.githubusercontent.com/75252077/152735363-9d5d21eb-bfff-4fc4-96f4-7ee9bfe7c9e2.mp4
--- wordoftheday/script.js
+++ wordoftheday/script.js
@@ -11,3 +11,16 @@ chrome.runtime.sendMessage({name: "fetchWords"}, (response) => {
   
   });
   
+  document.getElementById('next').addEventListener('click',()=>
+{
+  chrome.runtime.sendMessage({name: "fetchWords"}, (response) => {
+    //Wait for Response
+  
+    console.log(response);
+  
+    document.querySelector('h4').innerHTML = response.word;
+    document.querySelector('p').innerHTML = response.desc;
+  
+  
+  });
+});
