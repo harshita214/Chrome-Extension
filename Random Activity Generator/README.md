4daf1954c3fd3807999d425864abbce162ffbed8
Shreya Ghosh <sgshreya024@gmail.com>
2022-03-07 15:47:28 +0530

    CSS and ss of extension added

--- Random Activity Generator/README.md
+++ Random Activity Generator/README.md
@@ -1,6 +1,6 @@
 # **Random Activity Generator Extension**
 
-This extension generate new quotes every time we click the new quote button.
+This extension generate new activities every time we click on the new group/solo button.
 
 ## Technologies used:
 
@@ -8,15 +8,14 @@ This extension generate new quotes every time we click the new quote button.
 - CSS
 - JavaScript
 
-### Sometime we are frustrated while doing work. To feel better we need quotes so this extension will help in that situation.
+### Sometime we are bored in our monotonous life. This Random activity generator will suggest an awesome activity to try something new.
 
-### In this extension I used an quote api to fetch data when the button is clicked it will fetch the quote and visible on our screen using html,css and javascript.
+### In this extension I used bored api to fetch data when the option button is clicked it will fetch the activity suggestion and is visible on our screen using html,css and javascript.This extension provides options of a solo/group activity.
 
 ---
 
 ## Screenshots
 
-![Extension](./images/img0.png)
-![Extension](./images/img1.png)
-![Extension](./images/img2.png)
-![Extension](./images/img3.png)
+![Loading](./images/img0.JPG)
+![Solo Activity](./images/img1.JPG)
+![Group Activity](./images/img2.JPG)
--- Random Activity Generator/script.js
+++ Random Activity Generator/script.js
@@ -1,9 +1,47 @@
 const url = "http://www.boredapi.com/api/activity";
-fetch(url)
-  .then((data) => data.json())
-  .then((jokeData) => {
-    const jokeText = jokeData.activity;
-    const jokeElement = document.getElementById("jokeElement");
 
-    jokeElement.innerHTML = jokeText;
-  });
+var solo = document.getElementById("solobutton");
+var grp = document.getElementById("groupbutton");
+
+solo.addEventListener("click", () => {
+  checker("solo");
+});
+grp.addEventListener("click", () => {
+  checker("group");
+});
+
+function checker(input) {
+  var num = Math.floor(Math.random() * 3) + 2;
+  switch (input) {
+    case "solo":
+      fetch("http://www.boredapi.com/api/activity?participants=1")
+        .then((data) => data.json())
+        .then((activityData) => {
+          const activityText = activityData.activity;
+          const activityElement = document.getElementById("activityElement");
+
+          activityElement.innerHTML = activityText;
+        });
+      break;
+    case "group":
+      fetch(`http://www.boredapi.com/api/activity?participants=${num}`)
+        .then((data) => data.json())
+        .then((activityData) => {
+          const activityText = activityData.activity;
+          const activityElement = document.getElementById("activityElement");
+
+          activityElement.innerHTML = activityText;
+        });
+      break;
+    default:
+      fetch("http://www.boredapi.com/api/activity")
+        .then((data) => data.json())
+        .then((activityData) => {
+          const activityText = activityData.activity;
+          const activityElement = document.getElementById("activityElement");
+
+          activityElement.innerHTML = activityText;
+        });
+      break;
+  }
+}
