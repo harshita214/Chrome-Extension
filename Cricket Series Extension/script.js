abe108cb12e6e57f84c05b6cb73134e6904bdbd0
Shreya Ghosh <sgshreya024@gmail.com>
2022-03-10 20:49:11 +0530

    Cricket Series Extension implemented

---
+++ Cricket Series Extension/script.js
@@ -0,0 +1,51 @@
+fetch("https://cricket-live-score4.p.rapidapi.com/apis/series", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "cricket-live-score4.p.rapidapi.com",
+    "x-rapidapi-key": "8e8b0875e1msh3b60b0d6d24c50ap1f267ejsn6a95b9a0b89d",
+  },
+})
+  .then((data) => data.json())
+  .then((activityData) => {
+    const seriesData = activityData.data.current;
+    let cardContainer;
+
+    let createTaskCard = (task) => {
+      let card = document.createElement("div");
+      card.className = "col ";
+      console.log(task);
+      let cardBody = document.createElement("div");
+
+      let sdate = document.createElement("h5");
+      var parts = task["dates "];
+      sdate.innerText = parts;
+
+      let seriesName = document.createElement("h3");
+      seriesName.innerText = task.series_name;
+
+      let wmintemp = document.createElement("h3");
+      wmintemp.innerText = task["series_type "];
+
+      cardBody.appendChild(sdate);
+      cardBody.appendChild(seriesName);
+      cardBody.appendChild(wmintemp);
+
+      card.appendChild(cardBody);
+      cardContainer.appendChild(card);
+    };
+
+    const newArray = seriesData.slice(0, 5);
+    let initListOfTasks = () => {
+      if (cardContainer) {
+        document.getElementById("daily").replaceWith(cardContainer);
+        return;
+      }
+
+      cardContainer = document.getElementById("daily");
+      newArray.forEach((task) => {
+        createTaskCard(task);
+      });
+    };
+
+    initListOfTasks();
+  });
