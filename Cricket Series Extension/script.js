a1a807030631ee660e905e3ac0f199bb52d2290e
Shreya Ghosh <sgshreya024@gmail.com>
2022-03-11 08:00:46 +0530

    Current and Upcoming created

--- Cricket Series Extension/script.js
+++ Cricket Series Extension/script.js
@@ -1,51 +1,125 @@
-fetch("https://cricket-live-score4.p.rapidapi.com/apis/series", {
-  method: "GET",
-  headers: {
-    "x-rapidapi-host": "cricket-live-score4.p.rapidapi.com",
-    "x-rapidapi-key": "8e8b0875e1msh3b60b0d6d24c50ap1f267ejsn6a95b9a0b89d",
-  },
-})
-  .then((data) => data.json())
-  .then((activityData) => {
-    const seriesData = activityData.data.current;
-    let cardContainer;
+var current = document.getElementById("current");
+var upcoming = document.getElementById("upcoming");
 
-    let createTaskCard = (task) => {
-      let card = document.createElement("div");
-      card.className = "col ";
-      console.log(task);
-      let cardBody = document.createElement("div");
+current.addEventListener("click", () => {
+  checker("current");
+});
+upcoming.addEventListener("click", () => {
+  checker("upcoming");
+});
 
-      let sdate = document.createElement("h5");
-      var parts = task["dates "];
-      sdate.innerText = parts;
+function checker(input) {
+  switch (input) {
+    case "current":
+      fetch("https://cricket-live-score4.p.rapidapi.com/apis/series", {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "cricket-live-score4.p.rapidapi.com",
+          "x-rapidapi-key":
+            "8e8b0875e1msh3b60b0d6d24c50ap1f267ejsn6a95b9a0b89d",
+        },
+      })
+        .then((data) => data.json())
+        .then((activityData) => {
+          const seriesData = activityData.data.current;
+          let cardContainer;
 
-      let seriesName = document.createElement("h3");
-      seriesName.innerText = task.series_name;
+          let createTaskCard = (task) => {
+            let card = document.createElement("div");
+            card.className = "col ";
+            console.log(task);
+            let cardBody = document.createElement("div");
 
-      let wmintemp = document.createElement("h3");
-      wmintemp.innerText = task["series_type "];
+            let sdate = document.createElement("h5");
+            var parts = task["dates "];
+            sdate.innerText = parts;
 
-      cardBody.appendChild(sdate);
-      cardBody.appendChild(seriesName);
-      cardBody.appendChild(wmintemp);
+            let seriesName = document.createElement("h3");
+            seriesName.innerText = task.series_name;
 
-      card.appendChild(cardBody);
-      cardContainer.appendChild(card);
-    };
+            let wmintemp = document.createElement("h3");
+            wmintemp.innerText = task["series_type "];
 
-    const newArray = seriesData.slice(0, 5);
-    let initListOfTasks = () => {
-      if (cardContainer) {
-        document.getElementById("daily").replaceWith(cardContainer);
-        return;
-      }
+            cardBody.appendChild(sdate);
+            cardBody.appendChild(seriesName);
+            cardBody.appendChild(wmintemp);
 
-      cardContainer = document.getElementById("daily");
-      newArray.forEach((task) => {
-        createTaskCard(task);
-      });
-    };
+            card.appendChild(cardBody);
+            cardContainer.appendChild(card);
+          };
 
-    initListOfTasks();
-  });
+          const newArray = seriesData.slice(0, 5);
+          let initListOfTasks = () => {
+            if (cardContainer) {
+              document.getElementById("daily").replaceWith(cardContainer);
+              return;
+            }
+
+            cardContainer = document.getElementById("daily");
+            newArray.forEach((task) => {
+              createTaskCard(task);
+            });
+          };
+
+          initListOfTasks();
+        });
+      break;
+    case "upcoming":
+      fetch("https://cricket-live-score4.p.rapidapi.com/apis/series", {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "cricket-live-score4.p.rapidapi.com",
+          "x-rapidapi-key":
+            "8e8b0875e1msh3b60b0d6d24c50ap1f267ejsn6a95b9a0b89d",
+        },
+      })
+        .then((data) => data.json())
+        .then((activityData) => {
+          const seriesData = activityData.data.upcoming;
+          let cardContainer;
+
+          let createTaskCard = (task) => {
+            let card = document.createElement("div");
+            card.className = "col ";
+            console.log(task);
+            let cardBody = document.createElement("div");
+
+            let sdate = document.createElement("h5");
+            var parts = task["dates "];
+            sdate.innerText = parts;
+
+            let seriesName = document.createElement("h3");
+            seriesName.innerText = task.series_name;
+
+            let wmintemp = document.createElement("h3");
+            wmintemp.innerText = task["series_type "];
+
+            cardBody.appendChild(sdate);
+            cardBody.appendChild(seriesName);
+            cardBody.appendChild(wmintemp);
+
+            card.appendChild(cardBody);
+            cardContainer.appendChild(card);
+          };
+
+          const newArray = seriesData.slice(0, 5);
+          let initListOfTasks = () => {
+            if (cardContainer) {
+              document.getElementById("daily").replaceWith(cardContainer);
+              return;
+            }
+
+            cardContainer = document.getElementById("daily");
+            newArray.forEach((task) => {
+              createTaskCard(task);
+            });
+          };
+
+          initListOfTasks();
+        });
+      break;
+    default:
+      console.log("Fetching...");
+      break;
+  }
+}
