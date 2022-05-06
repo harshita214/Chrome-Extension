c29b827be0ea9743f0a41486eb8b0fe1746dda34
Akriti <vermaakriti700@gmail.com>
2022-05-06 22:23:01 +0530

    removed the error

--- Random Anime Pics/script.js
+++ Random Anime Pics/script.js
@@ -3,10 +3,9 @@ const animeBox = document.querySelector(".animeBox");
 const anime = document.querySelector(".anime");
 const animeName = document.querySelector(".heroName");
 
-const api_url = `https://api.catboys.com/img`;
 
 getbtn.addEventListener("click", async function () {
-  const res = await fetch(api_url);
+  const res = await fetch(`https://api.catboys.com/img`);
   const data = await res.json();
   anime.src = data.url;
   animeBox.style.display = "block";
