b1d69358bf50d4769b91ce522cde62ba0ffd3dcc
Vivek Kumar Tiwari <vivekchallenger20@gmail.com>
2023-03-08 20:58:58 +0530

    changing the toogle dark mode button UI

--- Dark Mode Extension/appOn.js
+++ Dark Mode Extension/appOn.js
@@ -1,8 +1,12 @@
+
 (function () {
 
-   document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-   let media = document.querySelectorAll("img, picture, video");
-   media.forEach((mediaItem) => {
-    mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
- });
+
+    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+    // document.body.style.filter = "invert(1) hue-rotate(180deg)"
+    let media = document.querySelectorAll("img, picture, video");
+    media.forEach((mediaItem) => {
+        mediaItem.style.filter = "invert(1) hue-rotate(180deg)"
+    })
+
 })();
