8f230232877cec98bb63044d56ad0c6ba9cbdd81
Lucky <theluckysingh9897@gmail.com>
2023-03-12 02:35:49 +0530

    Updated QR generator

--- QR-Generator/script.js
+++ QR-Generator/script.js
@@ -1,12 +1,12 @@
-let qrcode = document.querySelector("img");
-let text = document.querySelector("textarea");
-let btn = document.querySelector("button");
 
-btn.addEventListener("click",generateQR);
+
+
+
+document.querySelector("button").addEventListener("click",generateQR);
 
 function generateQR(){
-    let data = text.value;
+    let data = document.querySelector("textarea").value;
     let baseURL = 'https://api.qrserver.com/v1/create-qr-code/';
     let url = `${baseURL}?data=${data}`;
-    qrcode.src = url;
+    document.querySelector("img").src = url;
 }
